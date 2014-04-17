17b56e3d45919e7dd07d7536ded7a5dfc9491cca
kakusha <email@kakusha.net>
2014-04-17 16:44:07 +0900

    second commit

--- prog1.cpp
+++ prog1.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <GL/glut.h>
 
 #define MAXPOINTS 100
@@ -120,6 +121,17 @@ void motion(int x, int y) {
 	rubberband=1;
 }
 
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'q':
+	case 'Q':
+	case '\033':
+		exit(0);
+	default:
+		break;
+	}
+}
+
 void init(void) {
 	glClearColor(0.0, 0.0, 1.0, 1.0);
 }
@@ -134,6 +146,7 @@ int main(int argc, char *argv[]) {
 	glutReshapeFunc(resize);
 	glutMouseFunc(mouse);
 	glutMotionFunc(motion);
+	glutKeyboardFunc(keyboard);
 	init();
 	glutMainLoop();
 	return 0;
